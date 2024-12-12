159e68a0180b00628324059bdb3df966a064914f
nguyengiabaoptit <nguyengiabao2880@gmail.com>
2024-12-12 10:06:35 +0700

    upload bài 2

---
+++ bài 2/Untitled1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char chuoi = "Xin chao cac ban!";
+    int i;
+    for (i = 0; i < chuoi.length(); i++) {
+cout << chuoi[i] << " ";
+}
+cout << endl; 
+
+return 0;
+}
